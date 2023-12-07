f1d5fca888f1b61593efe680a9997269e4fcccb2
date3k2 <nguyenduydat047@gmail.com>
2023-12-08 03:48:25 +0700

    Refactor solve function to use pre-loaded graph

--- backend/api.py
+++ backend/api.py
@@ -1,21 +1,18 @@
 from flask import Flask, request
-import osmnx as ox
 from flask_cors import CORS
-import sys
-from algo.nearest_node import nearest_node
-from algo.djikstra import Graph
+from algo.graph import Graph
+import pickle
 
 app = Flask(__name__)
 CORS(app)
+with open("graph.pkl", "rb") as f:
+    g: Graph = pickle.load(f)
 
 
-def solve(start, end):
-    G = ox.graph_from_place("Hoàn Kiếm, Hà Nội, Vietnam", network_type="drive")
-    coords = [(data["y"], data["x"], key) for key, data in G.nodes(data=True)]
-    nr_start = nearest_node((start["lat"], start["lng"]), coords)
-    nr_end = nearest_node((end["lat"], end["lng"]), coords)
-    graph = Graph(G)
-    path = graph.dijkstra(nr_start, nr_end)
+def solve(g: Graph, start, end):
+    nearest_start_id = g.nearest_node((start["lat"], start["lng"]))
+    nearest_end_id = g.nearest_node((end["lat"], end["lng"]))
+    path = g.dijkstra(nearest_start_id, nearest_end_id)
     path.appendleft((start["lat"], start["lng"]))
     path.append((end["lat"], end["lng"]))
     return list(path)
@@ -31,7 +28,7 @@ def index():
         "lat": float(request.args.get("m2_lat")),
         "lng": float(request.args.get("m2_lng")),
     }
-    return solve(m1, m2)
+    return solve(g, m1, m2)
 
 
 app.run(debug=True)
